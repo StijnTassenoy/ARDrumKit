6b590ed152b4a6b16a352f341ad9d9e58bd9fdd8
StijnTassenoy <stijn.tassenoy@gmail.com>
2021-05-10 20:16:19 +0200

    Added Virtual buttons + Image Targets + Scripts

---
+++ AR-DRUM-KIT/Assets/Scripts/KickVBTNScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class KickVBTNScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    VirtualButtonBehaviour[] virtualButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("...START...");
+        virtualButtons = GetComponentsInChildren<VirtualButtonBehaviour>();
+        Debug.Log("virtualButtons.Length");
+        foreach (VirtualButtonBehaviour virtualButton in virtualButtons)
+        {
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("...KICK IS BEING KICKED...");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("...RELEASED...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ AR-DRUM-KIT/Assets/Scripts/SnareVBTNScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class SnareVBTNScript : MonoBehaviour, IVirtualButtonEventHandler
+{
+    VirtualButtonBehaviour[] virtualButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("...START...");
+        virtualButtons = GetComponentsInChildren<VirtualButtonBehaviour>();
+        Debug.Log("virtualButtons.Length");
+        foreach (VirtualButtonBehaviour virtualButton in virtualButtons)
+        {
+            virtualButton.RegisterEventHandler(this);
+        }
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("...SNARE IS BEING HIT...");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("...RELEASED...");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
